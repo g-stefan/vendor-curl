e496b0361b0e07a66accd9377995b41750b167a4
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:26:07 +0200

    Update

--- build/platform/msvc.vendor.cmd
+++ build/platform/msvc.vendor.cmd
@@ -3,6 +3,4 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> version-dependency-update vendor-curl
-
-xyo-cc --update-version-dependency vendor-curl
+echo - %BUILD_PROJECT% ^> vendor
